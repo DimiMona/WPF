3a699f677305c190c9f87cd1a9ccd5b116f3dd1a
Mona Dmitriy <skorobei91@mail.ru>
2026-04-29 22:04:59 +0300

    Сделал ДЗ подготовив метод который отвечет за нажатие клавиши ESC и Enter и выполняет действия указанные в ДЗ

--- CustomTextBoxControl/View/UserControls/ClearableTextBox.xaml.cs
+++ CustomTextBoxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -13,30 +13,67 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace CustomTextBoxControl.View.UserControls
 {
-	/// <summary>
-	/// Interaction logic for ClearableTextBox.xaml
-	/// </summary>
 	public partial class ClearableTextBox : UserControl
 	{
-		string placeholder;
+		string placeholder;//переменная где храним подсказки(Имя, Фамилия и т.д.)
 
+		//Свойство, которое позволяет задавать подсказку из XAML (Имя, Фамилия и т.д.)
 		public string Placeholder
 		{
-			get => placeholder; 
+			get => placeholder;
 			set => placeholder = tbPlaceholder.Text = value;
 		}
+
 		public ClearableTextBox()
 		{
-			InitializeComponent();
+			InitializeComponent();			
+			
+			this.PreviewKeyDown += ClearableTextBox_PreviewKeyDown;
+			txtInput.PreviewKeyDown += TxtInput_PreviewKeyDown;
 		}
 
-		private void btnClear_Click(object sender, RoutedEventArgs e) => txtInput.Text = "";
+		//Метод который отвечает за нажатие клавиши с проверкой
+		private void HandleKeyPress(KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape)
+			{
+				txtInput.Text = "";//если пользователь нажимает ESC очищается строка
+				e.Handled = true;//Говорит системе: "Я уже обработал эту клавишу, не нужно делать с ней ничего больше".
+
+			}
+			else if (e.Key == Key.Enter)
+			{
+				e.Handled = true;
+				//Найди элемент, на котором сейчас курсор
+				UIElement nextElement = (UIElement)Keyboard.FocusedElement;
+				//Перемести курсор на следующее поле ввода
+				nextElement.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+
+			}
+		}
+
+		
+		private void ClearableTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+		{
+			HandleKeyPress(e);
+		}
+
+		
+		private void TxtInput_PreviewKeyDown(object sender, KeyEventArgs e)
+		{
+			HandleKeyPress(e);
+		}
+
+		private void btnClear_Click(object sender, RoutedEventArgs e)
+		{
+			txtInput.Text = "";
+			txtInput.Focus();  
+		}
+
 		private void txtInput_TextChanged(object sender, TextChangedEventArgs e) =>
 			tbPlaceholder.Visibility = txtInput.Text == "" ? Visibility.Visible : Visibility.Hidden;
-
-
 	}
-
 }
